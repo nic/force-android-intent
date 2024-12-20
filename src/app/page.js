df6c1ea993cec6043ee3b9d4a8c974226ed480e8
Nic <nic@users.noreply.github.com>
2024-12-20 17:58:13 -0300

    Solving twice universal link call on Android with Chrome

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,36 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
+import {useState} from "react";
 
 export default function Home() {
+  const [pkg, setPkg] = useState("br.com.gabba.Caixa");
+  const [cefUrl, setcefUrl] = useState("");
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
+        <h1>Force Intent</h1>
+        <input value={pkg} onChange={e => setPkg(e.target.value)}/>
+        <input value={cefUrl} onChange={e => setcefUrl(e.target.value)} />
         <div className={styles.ctas}>
           <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+              href={`intent://${cefUrl.replaceAll('https://', '')}#Intent;scheme=https;package=${pkg};end`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.primary}
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            INTENT CALL
           </a>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
+              href={`${cefUrl}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.secondary}
           >
-            Read our docs
+            DIRECT CALL
           </a>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
